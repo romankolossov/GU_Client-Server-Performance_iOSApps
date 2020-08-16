a6b1fd4ad895ab5f0dfca81df1d8763c30476241
romankolossov <rkolossov@gmail.com>
2020-08-16 11:15:55 +0300

    Lesson3 home work started

--- vkAlissia-2/vkAlissia-2/AppViewController.swift
+++ vkAlissia-2/vkAlissia-2/AppViewController.swift
@@ -2,7 +2,7 @@
 //  AppViewController.swift
 //  vkAlissia-2
 //
-//  Created by Роман Колосов on 12.08.2020.
+//  Created by Роман Колосов on 16.08.2020.
 //  Copyright © 2020 Roman N. Kolosov. All rights reserved.
 //
 
@@ -11,12 +11,15 @@ import UIKit
 class AppViewController: UIViewController {
     
     let networkManager = NetworkManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         networkManager.getData(for: "groups")
-        networkManager.getData(for: "friends")
-        //networkManager.loadData(for: "photos")
     }
+
 }
