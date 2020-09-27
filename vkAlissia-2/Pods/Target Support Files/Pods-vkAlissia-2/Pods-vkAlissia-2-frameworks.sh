26e27be5bd0192566fa34922fafd13ea6003a78d
romankolossov <rkolossov@gmail.com>
2020-09-27 10:10:09 +0300

    PromiseKit installed

--- vkAlissia-2/Pods/Target Support Files/Pods-vkAlissia-2/Pods-vkAlissia-2-frameworks.sh
+++ vkAlissia-2/Pods/Target Support Files/Pods-vkAlissia-2/Pods-vkAlissia-2-frameworks.sh
@@ -197,11 +197,13 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
