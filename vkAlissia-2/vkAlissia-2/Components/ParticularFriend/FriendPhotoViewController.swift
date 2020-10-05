89915129a162f9b8cd2402098ab678632d76fdb1
romankolossov <rkolossov@gmail.com>
2020-10-05 23:12:55 +0300

    Perf_Lesson6 home work in progress

--- vkAlissia-2/vkAlissia-2/Components/ParticularFriend/FriendPhotoViewController.swift
+++ vkAlissia-2/vkAlissia-2/Components/ParticularFriend/FriendPhotoViewController.swift
@@ -45,6 +45,7 @@ class FriendPhotoViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         nameLabel.textColor = .black
+        nameLabel.backgroundColor = .white
         nameLabel.font = .preferredFont(forTextStyle: .title1)
         nameLabel.textAlignment = .center
         
